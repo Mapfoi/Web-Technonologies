de66399cc827e76a51104281f4c0caaf5263baea
Mapfoi <seva.eshmanskii1109@gmail.com>
2026-03-21 19:13:32 +0300

    1 and 2 ready

---
+++ Lab2/task_01.py
@@ -0,0 +1,28 @@
+# 1.	Чтение из командной строки произвольного набора
+# параметров и определение для каждого параметра типа
+# данных (дробное число, целое число, строка).
+
+while True:
+    inp = input("Enter something or 'exit': ")
+
+    if inp.lower() == 'exit':
+        break
+
+    try:
+        int_var = int(inp)
+        print("input is int")
+        continue
+    except ValueError:
+        pass
+
+    try:
+        float_var = float(inp)
+        print("input is float")
+        continue
+    except ValueError:
+        pass
+
+    try:
+        print("input is string")
+    except ValueError:
+        pass
